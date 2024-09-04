ef7f12e3095a6926ca80aaf5f0fe395dc31ce1f5
Alex Söderberg <as228gc@student.lnu.se>
2024-09-04 15:05:42 +0200

    Add encrypt and decrypt methods

---
+++ src/js/app.js
@@ -0,0 +1,33 @@
+const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'å', 'ä', 'ö']
+
+/**
+ * Encrypts a string by an offset.
+ *
+ * @param {string} decrypted - The string to be encrypted.
+ * @param {number} offset - By which number the character should be offset
+ * @returns {string} - The encrypted string
+ */
+function encrypt (decrypted, offset = 2) {
+  const encrypted = []
+  const lowerCase = decrypted.toLowerCase()
+  for (let i = 0; i < lowerCase.length; i++) {
+    console.log(alphabet.indexOf(lowerCase[i]))
+    encrypted[i] = alphabet[alphabet.indexOf(lowerCase[i]) + offset]
+  }
+  return encrypted.join('')
+}
+
+const userInput = prompt('Enter your name')
+let isToggled = false
+const button = document.getElementById('toggle')
+button.addEventListener('click', () => {
+  const nameDisplay = document.getElementById('name')
+  if (!isToggled) {
+    nameDisplay.innerText = encrypt(userInput)
+  } else {
+    nameDisplay.innerText = userInput
+  }
+  isToggled = !isToggled
+  console.log(isToggled)
+  console.log(encrypt(userInput))
+})
